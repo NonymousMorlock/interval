c47de6081648e47630e212c709b40a9f4460d6d5
NonymousMorlock <agamspaul@gmail.com>
2024-07-26 02:51:06 +0000

    🧹 Refactor: Parse the assets into a dart object

---
+++ lib/core/res/media.dart
@@ -0,0 +1,11 @@
+abstract class Media {
+  static const _baseIcons = 'assets/icons';
+  static const _baseLottie = 'assets/lottie';
+  static const _baseRive = 'assets/rive';
+
+  static const logo$icon = '$_baseIcons/logo.png';
+
+  static const gears$lottie = '$_baseLottie/gears.json';
+
+  static const funTime$rive = '$_baseRive/fun_time.riv';
+}
