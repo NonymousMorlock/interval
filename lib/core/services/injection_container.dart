9407f680963dfdd1651e52c616f718019a3c7df6
NonymousMorlock <agamspaul@gmail.com>
2024-07-27 22:14:48 +0000

    ✨ Feat: Register the database and SharedPreferences and the Home feature dependencies

--- lib/core/services/injection_container.dart
+++ lib/core/services/injection_container.dart
@@ -1,4 +1,8 @@
 import 'package:get_it/get_it.dart';
+import 'package:interval/core/services/database_helper.dart';
+import 'package:interval/core/singletons/current_platform.dart';
+import 'package:interval/src/home/repositories/home_local_repository.dart';
+import 'package:interval/src/home/view_models/home_view_model_cubit.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 part 'injection_container.main.dart';
--- lib/core/services/injection_container.main.dart
+++ lib/core/services/injection_container.main.dart
@@ -3,7 +3,25 @@ part of 'injection_container.dart';
 final sl = GetIt.instance;
 
 Future<void> init() async {
-  final prefs = await SharedPreferences.getInstance();
+  await _initMemory();
+  await _initHome();
+}
 
+Future<void> _initHome() async {
+  sl
+    ..registerFactory(() => HomeViewModelCubit(sl()))
+    ..registerLazySingleton(() => HomeLocalRepository(sl()));
+}
+
+Future<void> _initMemory() async {
+  final prefs = await SharedPreferences.getInstance();
   sl.registerLazySingleton(() => prefs);
+
+  if (CurrentPlatform.instance.isDesktop) {
+    final database = await DatabaseHelper.initDatabaseDesktop();
+    sl.registerLazySingleton(() => database);
+  } else {
+    final database = await DatabaseHelper.initDatabaseMobile();
+    sl.registerLazySingleton(() => database);
+  }
 }
