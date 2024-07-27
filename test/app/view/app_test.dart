4f4a10cd7c0ed9ae3e48fc0ac45aac3de9f556c4
NonymousMorlock <agamspaul@gmail.com>
2024-07-27 22:00:11 +0000

    🧪 Test: Write widget test for home_page.dart

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:interval/app/app.dart';
-import 'package:interval/src/home/views/home_view.dart';
+import 'package:interval/src/home/views/pages/home_page.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders HomeView', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(HomeView), findsOneWidget);
+      expect(find.byType(HomePage), findsOneWidget);
     });
   });
 }
