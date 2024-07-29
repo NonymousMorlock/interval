b967dc0686dcc754d0eb2467ddb9f6dff55f9eb9
NonymousMorlock <agamspaul@gmail.com>
2024-07-29 00:44:22 +0000

    🧹 Refactor: Create a typedef for Map<String, dynamic> type

--- lib/core/utils/typedefs.dart
+++ lib/core/utils/typedefs.dart
@@ -2,3 +2,4 @@ import 'package:dartz/dartz.dart';
 import 'package:interval/core/errors/failure.dart';
 
 typedef ResultFuture<T> = Future<Either<Failure, T>>;
+typedef DataMap = Map<String, dynamic>;
