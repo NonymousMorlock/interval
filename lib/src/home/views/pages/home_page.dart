f73305de7606370053a10db56d67e5df236217ce
NonymousMorlock <agamspaul@gmail.com>
2024-07-29 00:49:33 +0000

    ✨ Feat: Add functionality to the FloatingActionButton on the HomePage

--- lib/src/home/views/pages/home_page.dart
+++ lib/src/home/views/pages/home_page.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
 import 'package:interval/core/common/models/interval_session.dart';
 import 'package:interval/core/singletons/current_platform.dart';
 import 'package:interval/core/utils/core_utils.dart';
 import 'package:interval/src/home/view_models/home_view_model_cubit.dart';
 import 'package:interval/src/home/views/pages/responsive/mobile.dart';
 import 'package:interval/src/home/views/widgets/theme_toggle.dart';
+import 'package:interval/src/manage_interval/views/pages/create_interval_session_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -54,12 +56,16 @@ class _HomePageState extends State<HomePage> {
       ),
       floatingActionButton: CurrentPlatform.instance.isDesktop
           ? FloatingActionButton.extended(
-              onPressed: () {},
+              onPressed: () {
+                // TODO(Create-Session): Open dialog to create interval session
+              },
               label: const Text('Create Interval Session'),
               icon: const Icon(Icons.add),
             )
           : FloatingActionButton(
-              onPressed: () {},
+              onPressed: () {
+                context.push(CreateIntervalSessionPage.path);
+              },
               child: const Icon(Icons.add),
             ),
     );
