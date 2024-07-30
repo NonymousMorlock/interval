e484a2d4940f1d816f79e8e8d51daab4a774e55a
NonymousMorlock <agamspaul@gmail.com>
2024-07-30 18:25:53 +0000

    ✨ Feat: Wrap the widget tree in an AppStateReactor

--- lib/core/services/router.dart
+++ lib/core/services/router.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:interval/core/common/widgets/app_state_reactor.dart';
 import 'package:interval/core/services/injection_container.dart';
 import 'package:interval/src/home/view_models/home_view_model_cubit.dart';
 import 'package:interval/src/home/views/pages/home_page.dart';
+import 'package:interval/src/manage_interval/views/pages/create_or_update_interval_session.dart';
 
 part 'router.main.dart';
--- lib/core/services/router.main.dart
+++ lib/core/services/router.main.dart
@@ -2,12 +2,23 @@ part of 'router.dart';
 
 final router = GoRouter(
   routes: [
-    GoRoute(
-      path: '/',
-      builder: (_, __) => BlocProvider(
-        create: (_) => sl<HomeViewModelCubit>(),
-        child: const HomePage(),
-      ),
+    ShellRoute(
+      builder: (_, __, child) {
+        return AppStateReactor(child: child);
+      },
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (_, __) => BlocProvider(
+            create: (_) => sl<HomeViewModelCubit>(),
+            child: const HomePage(),
+          ),
+        ),
+        GoRoute(
+          path: CreateOrUpdateIntervalSessionPage.path,
+          builder: (_, __) => const CreateOrUpdateIntervalSessionPage(),
+        ),
+      ],
     ),
   ],
 );
