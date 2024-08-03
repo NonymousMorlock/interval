b50a7fd7e352c33313ad1cc3a0db170fd9d83577
NonymousMorlock <agamspaul@gmail.com>
2024-08-03 17:06:17 +0000

    ✨ Feat: Create a Widget for the fun time Animation

---
+++ lib/src/session/views/widgets/fun_time.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:interval/core/common/app/current_theme_mode.dart';
+import 'package:interval/core/extensions/context_extensions.dart';
+import 'package:interval/core/res/media.dart';
+import 'package:rive/rive.dart';
+
+class FunTime extends StatelessWidget {
+  const FunTime({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: CurrentThemeMode.instance,
+      builder: (_, themeMode, __) {
+        if (themeMode == ThemeMode.dark ||
+            (themeMode == ThemeMode.system && context.isDarkMode)) {
+          return const RiveAnimation.asset(Media.funTimeDark$rive);
+        }
+        return const RiveAnimation.asset(Media.funTime$rive);
+      },
+    );
+  }
+}
