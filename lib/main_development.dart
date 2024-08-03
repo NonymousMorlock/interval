7a700173bac478c73cf701f0b7b94589352eb6d0
NonymousMorlock <agamspaul@gmail.com>
2024-08-03 17:16:04 +0000

    ✅ Build: Wrap the Development App with DevicePreview when it's on Desktop

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,13 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:interval/app/app.dart';
 import 'package:interval/bootstrap.dart';
+import 'package:interval/core/singletons/current_platform.dart';
 
 void main() {
-  bootstrap(() => const App());
+  bootstrap(
+    () => DevicePreview(
+      enabled: CurrentPlatform.instance.isDesktop,
+      builder: (_) => const App(),
+    ),
+  );
 }
